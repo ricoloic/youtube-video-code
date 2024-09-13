5805496917dc20d95d392e3e0d45a4f975ada55d
ricoloic <ricoloic4@gmail.com>
2024-09-13 10:26:17 -0400

    updated the sinewave-repetition sketch

--- p5/sinewave-repetition/sketch.js
+++ p5/sinewave-repetition/sketch.js
@@ -1,6 +1,6 @@
 var config = {
     sectionCount: 10,
-    colors: randomPalette(),
+    colors: COLOR_PALETTES.deep, //randomPalette(),
 };
 
 function setup() {
@@ -17,20 +17,26 @@ function setup() {
 
     const scl = height / (config.sectionCount);
 
+    let colorIndex = 0;
     for (let i = 0; i < config.sectionCount; i++) {
         const y = scl * (i + 1);
-        let colorIndex = 0;
+        const nextColorIndex = colorIndex + 1
         const start = random(0, 100);
         const end = start + random(20, 30);
         for (let x = 0; x < width; x += 20) {
+            const amt = random(0, 1);
+            const alpha = random(180, 255);
+            const c1 = config.colors[colorIndex % config.colors.length].rgba;
+            const c2 = config.colors[nextColorIndex % config.colors.length].rgba;
+            const c = lerpColor(color(c1.r, c1.g, c1.b, alpha), color(c2.r, c2.g, c2.b, alpha), amt);
             const v = map(x, 0, width, start, end);
-            const radius = map(sin(v), -1, 1, 0.5, 1) * 100 + random(-10, 20);
-            fill(config.colors[colorIndex % config.colors.length].color);
+            const radius = map(sin(v), -1, 1, 0.5, 1) * 100 + random(-10, 20) * map(i, 0, config.sectionCount, 1, 7);
+            fill(c);
             arc(x, y, radius*2, radius*2, PI, TWO_PI);
             point(x, y - radius);
-            colorIndex++;
         }
         line(0, y, width, y);
+        colorIndex++;
     }
 }
 
