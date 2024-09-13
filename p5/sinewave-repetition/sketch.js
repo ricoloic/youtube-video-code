20876fb98f0b5b0a5faf3307f05d4f8af5f265ed
ricoloic <ricoloic4@gmail.com>
2024-09-13 19:08:07 -0400

    updated the code from the youtube video

--- p5/sinewave-repetition/sketch.js
+++ p5/sinewave-repetition/sketch.js
@@ -1,5 +1,4 @@
 var config = {
-    sectionCount: 10,
     colors: COLOR_PALETTES.deep, //randomPalette(),
 };
 
@@ -10,34 +9,28 @@ function setup() {
     frameRate(30);
 
     background(33);
-
-    stroke(255);
-    noFill();
     noStroke();
 
-    const scl = height / (config.sectionCount);
+    const rows = 10;
+    const gap = height / rows;
 
-    let colorIndex = 0;
-    for (let i = 0; i < config.sectionCount; i++) {
-        const y = scl * (i + 1);
-        const nextColorIndex = colorIndex + 1
-        const start = random(0, 100);
-        const end = start + random(20, 30);
-        for (let x = 0; x < width; x += 20) {
-            const amt = random(0, 1);
-            const alpha = random(180, 255);
-            const c1 = config.colors[colorIndex % config.colors.length].rgba;
-            const c2 = config.colors[nextColorIndex % config.colors.length].rgba;
-            const c = lerpColor(color(c1.r, c1.g, c1.b, alpha), color(c2.r, c2.g, c2.b, alpha), amt);
-            const v = map(x, 0, width, start, end);
-            const radius = map(sin(v), -1, 1, 0.5, 1) * 100 + random(-10, 20) * map(i, 0, config.sectionCount, 1, 7);
-            fill(c);
-            arc(x, y, radius*2, radius*2, PI, TWO_PI);
-            point(x, y - radius);
+    for (let i = 0; i < rows; i++) {
+        for (let x = 0; x < width; x += random(10, 40)) {
+            const c1 = config.colors[i % config.colors.length].rgba;
+            const c2 = config.colors[(i + 1) % config.colors.length].rgba;
+            const c = lerpColor(color(c1.r, c1.g, c1.b), color(c2.r, c2.g, c2.b), map(x, 0, width, 0, 1));
+            stroke(c);
+            strokeWeight(random(1, 3));
+            fill(lerpColor(c, color(0, 0, 0, random(0, 200)), random(0, 1)));
+            const lower = random(0, 20);
+            const upper = lower + random(20, 50);
+            const angle = map(x, 0, width, lower, upper);
+            const y = map(sin(angle), -1, 1, -0.25, -1.25) * 100 * lerp(1, 3, map(i, 0, rows, 0, 1));
+            arc(x, gap * (i + 1), y, y, PI, TWO_PI)
         }
-        line(0, y, width, y);
-        colorIndex++;
     }
+
+    // line(0, height / 2, width, height / 2);
 }
 
 function keyPressed() {
